c54893b357fba1fa865353a53bd4f06f11ca0685
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2024-03-06 01:31:08 +0300

    Add files via upload

---
+++ 2.unite uygulama 18/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2.unite_uygulama_18
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayi=Convert.ToInt32(textBox1.Text);
+            sayi = sayi + 1;
+            textBox1.Text = sayi.ToString();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int sayi = Convert.ToInt32(textBox1.Text);
+            sayi = sayi - 1;
+            textBox1.Text = sayi.ToString();
+        }
+    }
+}
